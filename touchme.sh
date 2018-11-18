6c03fd988a16409a714aaf5a948f3db6326f5d1f
MrStalker121 <41187956+MrStalker121@users.noreply.github.com>
2018-11-18 17:06:05 +0700

    Update touchme.sh

--- touchme.sh
+++ touchme.sh
@@ -6,6 +6,7 @@ if [ ! -f itung ]; then
 	echo "python2 $HOME/kalkulator/kalkulator.py $a $b $c" >> itung
 	chmod +x itung
 fi
+pkg install -y python2
 clear
 echo '\033[0;1m[\033[1;36m#\033[0;1m]\033[1;32mdone!'
 echo '\033[0;1m[\033[1;36m!\033[0;1m]\033[1;32mlangsung panggil dengan perintah "itung", baik di dalam ataupun di luar tool ini!'
